bc610b08d1e35481cb1493fc5da0387233abb249
hilmanzu <hilmanzutech@gmail.com>
2025-02-12 02:19:47 +0700

    feat:

--- src/screens/FastRide/ChoiceScreen.js
+++ src/screens/FastRide/ChoiceScreen.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity, ScrollView, Alert, PermissionsAndroid, Platform, Modal, Pressable } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import 'react-native-get-random-values';
+import Geocoder from 'react-native-geocoding';
+import axios from 'axios';
+import { dispatchCommand } from 'react-native-reanimated';
 
 const GOOGLE_API_KEY = 'AIzaSyBpcZDAU9DmCZqBGwpHpGxw7mcGq7Q75D8'; // Ganti dengan API Key Anda
 
@@ -30,14 +33,18 @@ const mapDUmmy = [
 
 const ChoiceScreen = () => {
 
-  const [latitudeFrom, setlatitudeFrom] = useState(0);
-  const [longitudeFrom, setlongitudeFrom] = useState(0);
+  const [distance, setDistance] = useState(null);  // Menyimpan jarak
+  const [duration, setDuration] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const [latitudeTo, setlatitudeTo] = useState(0);
-  const [longitudeTo, setlongitudeTo] = useState(0);
 
   const [from, setfrom] = useState('Pilih Penjemputan');
   const [destination, setdestination] = useState('Pilih Pengantaran');
+
+  const maxLength = 30;
+  const truncatedFromText = from.length > maxLength ? from.substring(0, maxLength) + "..." : from;
+  const truncatedDesText = destination.length > maxLength ? destination.substring(0, maxLength) + "..." : destination;
+  const [coordinates, setCoordinates] = useState([]);
 
   const [location, setLocation] = useState(null);
 
@@ -72,8 +79,8 @@ const ChoiceScreen = () => {
   });
 
   const [destinationLocation, setDestinationLocation] = useState({
-    latitude: 1.047237,
-    longitude: 103.992613,
+    latitude: 0,
+    longitude: 0,
   });
 
   // Request permissions on Android
@@ -97,7 +104,11 @@ const ChoiceScreen = () => {
     Geolocation.getCurrentPosition(
       (position) => {
         setLocation(position.coords);
-        setPickupLocation({latitude:position.coords.latitude, longitude:position.coords.longitude});
+        setPickupLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+        setDestinationLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+        getLocationName({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+        setRegionPick({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+        setLoading(false)
       },
       (error) => {
         Alert.alert('Error', `Unable to get location: ${error.message}`);
@@ -105,9 +116,83 @@ const ChoiceScreen = () => {
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     );
   };
+  Geocoder.init('AIzaSyBpcZDAU9DmCZqBGwpHpGxw7mcGq7Q75D8');
+
+
+  const getLocationName = (latitude, longitude) => {
+    Geocoder.from(latitude, longitude)
+      .then(json => {
+        const address = json.results[0].formatted_address;
+        setfrom(address); // Menyimpan alamat ke state
+      })
+      .catch(error => console.warn(error));
+  };
+
+  const getLocationDesName = (latitude, longitude) => {
+    Geocoder.from(latitude, longitude)
+      .then(json => {
+        const address = json.results[0].formatted_address;
+        setdestination(address); // Menyimpan alamat ke state
+      })
+      .catch(error => console.warn(error));
+  };
+
+  const decodePolyline = (encoded) => {
+    let points = [];
+    let index = 0;
+    let len = encoded.length;
+    let lat = 0;
+    let lng = 0;
+
+    while (index < len) {
+      let b, shift = 0, result = 0;
+      do {
+        b = encoded.charCodeAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      let dlat = ((result & 0x01) !== 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.charCodeAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      let dlng = ((result & 0x01) !== 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      points.push({ latitude: (lat / 1E5), longitude: (lng / 1E5) });
+    }
+    return points;
+  };
+
+  const fetchRoute = async () => {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${pickupLocation.latitude},${pickupLocation.longitude}&destination=${destinationLocation.latitude},${destinationLocation.longitude}&key=${GOOGLE_API_KEY}`
+      );
+
+      const points = response.data.routes[0].overview_polyline.points;
+      const decodedPoints = decodePolyline(points);
+      setCoordinates(decodedPoints);
+
+      // Mendapatkan jarak dan durasi perjalanan
+      const distanceText = response.data.routes[0].legs[0].distance.text;
+      const durationText = response.data.routes[0].legs[0].duration.text;
+
+      setDistance(distanceText);  // Menyimpan jarak
+      setDuration(durationText);  // Menyimpan durasi
+    } catch (error) {
+      console.error("Error fetching route: ", error);
+    }
+  };
 
   useEffect(() => {
     getCurrentLocation();
+    // getLocationName({ latitude: pickupLocation.latitude, longitude: pickupLocation.longitude });
   }, []);
 
   const handleLocationPickup = (data, details) => {
@@ -121,7 +206,7 @@ const ChoiceScreen = () => {
       });
       setPickupLocation({ latitude: lat, longitude: lng });
       setfrom(data.description)
-      updateMap()
+
       mapRef.current?.animateToRegion({
         latitude: lat,
         longitude: lng,
@@ -142,7 +227,7 @@ const ChoiceScreen = () => {
       });
       setDestinationLocation({ latitude: lat, longitude: lng });
       setdestination(data.description)
-      updateMap()
+
       mapRef.current?.animateToRegion({
         latitude: lat,
         longitude: lng,
@@ -157,38 +242,27 @@ const ChoiceScreen = () => {
       latitude: newRegion.latitude,
       longitude: newRegion.longitude,
     });
-    updateMap()
   };
 
-  
+
 
   const handleRegionFromComplete = (newRegion) => {
     setPickupLocation({
       latitude: newRegion.latitude,
       longitude: newRegion.longitude,
     });
-    updateMap()
-  };
-
-  const updateMap = () => {
-    setRegion({
-      latitude: pickupLocation.latitude,
-      longitude: pickupLocation.longitude,
-      latitudeDelta: 0.015,
-      longitudeDelta: 0.0121,
-    });
-    setPickupLocation({
-      latitude: pickupLocation.latitude, // Ganti dengan koordinat baru
-      longitude: pickupLocation.longitude,
-    });
-
-    setDestinationLocation({
-      latitude: destinationLocation.latitude, // Ganti dengan koordinat baru
-      longitude: destinationLocation.longitude,
-    });
   };
 
   const mapRef = useRef(null);
+
+  if(loading)
+  {
+    return(
+      <View style={styles.containerloading}>
+        <Text>Sedang Menentukan Lokasi</Text>
+      </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
@@ -200,7 +274,7 @@ const ChoiceScreen = () => {
                 source={require('../../asset/from.png')}  // Local image
                 style={{ width: 20, height: 20, marginRight: 10 }}
               />
-              <Text>{from}</Text>
+              <Text>{truncatedFromText}</Text>
             </View>
           </TouchableOpacity>
           <View style={{ width: width - 80, height: 1, backgroundColor: '#00000020', marginBottom: 10, marginTop: 10 }} />
@@ -210,52 +284,81 @@ const ChoiceScreen = () => {
                 source={require('../../asset/des.png')}  // Local image
                 style={{ width: 20, height: 20, marginRight: 10 }}
               />
-              <Text>{destination}</Text>
+              <Text>{truncatedDesText}</Text>
             </View>
           </TouchableOpacity>
         </View>
         <View style={{ margin: 10 }} />
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: pickupLocation.latitude,
-            longitude: pickupLocation.longitude,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-          <Marker coordinate={pickupLocation} pinColor='red' />
-          <Marker coordinate={destinationLocation} pinColor='green'/>
-        </MapView>
-        <View style={{ margin: 10 }} />
-        <View style={{ width: 30, height: 1, backgroundColor: 'black' }} />
-        <View style={{ flex: 3, width: width - 40, alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
-          {mapDUmmy.map((data, index) => {
-            return (
-              <TouchableOpacity key={index} style={{ justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, width: 100, height: 100, backgroundColor: '#37AFE110', borderRadius: 10, borderColor: '#37AFE1', borderWidth: 1 }}>
-                {data.image}
-                <Text style={{ fontSize: 10 }}>{data.name}</Text>
-                <Text>Rp 25.000</Text>
-              </TouchableOpacity>
-            );
-          })}
+        {
+          destination !== "Pilih Pengantaran" &&
+          <>
+            <MapView
+              provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+              style={styles.map}
+              region={{
+                latitude: pickupLocation.latitude,
+                longitude: pickupLocation.longitude,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              }}
+            >
+              <Marker coordinate={pickupLocation} pinColor='red' />
+              <Marker coordinate={destinationLocation} pinColor='green' />
+              <Polyline
+                coordinates={coordinates}
+                strokeColor="#37AFE1"   // Warna garis
+                strokeWidth={4}      // Ketebalan garis
+              />
+            </MapView>
+            <View style={{ margin: 10 }} />
+            <View style={{ width: 30, height: 1, backgroundColor: 'black' }} />
+            <View style={{ flex: 3, width: width - 40, alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
+              {mapDUmmy.map((data, index) => {
+                return (
+                  <TouchableOpacity key={index} style={{ justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, width: 100, height: 100, backgroundColor: '#37AFE110', borderRadius: 10, borderColor: '#37AFE1', borderWidth: 1 }}>
+                    {data.image}
+                    <Text style={{ fontSize: 10 }}>{data.name}</Text>
+                    <Text>Rp 25.000</Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+            <Text style={styles.textPayment}>{distance} {duration}</Text>
+            <View style={{ margin: 10 }} />
+          </>
+        }
+        {
+          destination === "Pilih Pengantaran" &&
+          <>
+            <View
+              style={styles.map}
+            >
+            </View>
+            <View
+              style={{ width: width, alignItems: 'center', justifyContent: 'center', padding: 150 }}
+            >
+            </View>
+          </>
+        }
+
+      </View>
+      {
+        destination !== "Pilih Pengantaran" &&
+        <View style={{ backgroundColor: '#37AFE110', width: width, alignItems: 'center', justifyContent: 'center', padding: 20 }}>
+          <TouchableOpacity style={styles.buttonPayment}>
+            <View>
+              <Text style={styles.textPayment}>Tunai </Text>
+              <Text style={styles.textPayment2}>Saldo : Rp 8.500</Text>
+            </View>
+            <Text style={styles.textPayment}>IDR 25.000</Text>
+          </TouchableOpacity>
+          <View style={{ margin: 5 }} />
+          <TouchableOpacity style={styles.buttonConfirm}>
+            <Text style={styles.textButton}>Pesan Sekarang</Text>
+          </TouchableOpacity>
+          <View style={{ margin: 5 }} />
         </View>
-      </View>
-      <View style={{ backgroundColor: '#37AFE110', width: width, alignItems: 'center', justifyContent: 'center', padding: 20 }}>
-        <TouchableOpacity style={styles.buttonPayment}>
-          <View>
-            <Text style={styles.textPayment}>Tunai</Text>
-            <Text style={styles.textPayment2}>Saldo : Rp 8.500</Text>
-          </View>
-          <Text style={styles.textPayment}>IDR 25.000</Text>
-        </TouchableOpacity>
-        <View style={{ margin: 5 }} />
-        <TouchableOpacity style={styles.buttonConfirm}>
-          <Text style={styles.textButton}>Pesan Sekarang</Text>
-        </TouchableOpacity>
-        <View style={{ margin: 5 }} />
-      </View>
+      }
 
       <Modal
         animationType="fade"
@@ -267,7 +370,7 @@ const ChoiceScreen = () => {
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <GooglePlacesAutocomplete
-              placeholder={from}
+              placeholder={truncatedFromText}
               fetchDetails={true}
               onPress={handleLocationPickup}
               query={{
@@ -281,7 +384,7 @@ const ChoiceScreen = () => {
             />
             <MapView
               provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-              style={{width:width,height:height, borderRadius:20}}
+              style={{ width: width, height: height, borderRadius: 20 }}
               region={{
                 latitude: regionPick.latitude,
                 longitude: regionPick.longitude,
@@ -305,7 +408,7 @@ const ChoiceScreen = () => {
             </TouchableOpacity>
             <Pressable
               style={[styles.button, styles.buttonClose]}
-              onPress={() => {setModalVisible(!modalVisible);updateMap()}}>
+              onPress={() => { setModalVisible(!modalVisible); getLocationName({ latitude: pickupLocation.latitude, longitude: pickupLocation.longitude }); fetchRoute() }}>
               <Text style={styles.textStyle}>Kembali</Text>
             </Pressable>
           </View>
@@ -322,7 +425,7 @@ const ChoiceScreen = () => {
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <GooglePlacesAutocomplete
-              placeholder={destination}
+              placeholder={truncatedDesText}
               fetchDetails={true}
               onPress={handleLocationDestination}
               query={{
@@ -336,7 +439,7 @@ const ChoiceScreen = () => {
             />
             <MapView
               provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-              style={{width:width,height:height, borderRadius:20}}
+              style={{ width: width, height: height, borderRadius: 20 }}
               region={{
                 latitude: regionDes.latitude,
                 longitude: regionDes.longitude,
@@ -360,7 +463,7 @@ const ChoiceScreen = () => {
             </TouchableOpacity>
             <Pressable
               style={[styles.button, styles.buttonClose]}
-              onPress={() => {setModalDesVisible(!modalDesVisible);updateMap()}}>
+              onPress={() => { setModalDesVisible(!modalDesVisible); getLocationDesName({ latitude: destinationLocation.latitude, longitude: destinationLocation.longitude }); fetchRoute() }}>
               <Text style={styles.textStyle}>Kembali</Text>
             </Pressable>
           </View>
@@ -375,6 +478,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: 'white',
+  },
+  containerloading: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent:'center'
   },
   title: {
     fontSize: 24,
@@ -445,8 +554,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#2196F3',
     width: width - 40,
     zIndex: 1,
-    position:'absolute',
-    marginTop:height-80
+    position: 'absolute',
+    marginTop: height - 80
   },
   textStyle: {
     color: 'white',
@@ -460,7 +569,7 @@ const styles = StyleSheet.create({
   autocompleteContainer: {
     width: width - 40,
     zIndex: 9999,
-    position:'absolute'
+    position: 'absolute'
   },
   textInput: {
     height: 50,
@@ -468,7 +577,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     fontSize: 16,
-    marginTop:30,
+    marginTop: 30,
     borderRadius: 10, borderColor: 'red', borderWidth: 1
   },
 
